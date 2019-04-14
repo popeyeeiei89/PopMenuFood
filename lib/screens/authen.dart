e900ff5ed1967cbdf268016c40f20f0a1681a092
masterUNG <master@abc.com>
2019-04-14 00:49:40 -0700

    chang BG ok

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,19 +6,36 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+// Show logo
+  Widget showlogo() {
+    return Image.asset('images/logo.png');
+  }
+
 // App Name
   Widget showAppName() {
-    return Text('Pop Restaurant');
+    return Text(
+      'Pop Restaurant',
+      style: TextStyle(
+          fontSize: 50.0, fontWeight: FontWeight.bold, color: Colors.white),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.orange, Colors.pink], begin: Alignment(0, 1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment.topCenter,
         child: Column(
-          children: <Widget>[showAppName(), showAppName(), showAppName()],
+          children: <Widget>[
+            showlogo(),
+            Container(margin: EdgeInsets.only(top: 30.0),
+              child: showAppName(),
+            )
+          ],
         ),
       ),
     );
